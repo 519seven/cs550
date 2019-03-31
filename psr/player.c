aca69037d77f149e1e939feb1547662cb39d64e0
519Seven <519seven@users.noreply.github.com>
2019-03-31 00:32:13 -0400

    after a little bit of cleanup

--- psr/player.c
+++ psr/player.c
@@ -8,7 +8,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <arpa/inet.h>
-#include <time.h>				// srand
+#include <time.h>														// srand
 
 /* Copyright Peter Akey 2019 */
 /* player for paper, rock, scissors */
@@ -18,15 +18,14 @@
 #define BUF_SIZE 500
 
 long random_at_most(long m);
-void readstr(int fd, char *buffer, size_t len);
-int writestr(int fd, char *message);
 
 const char hostname[] = "localhost";
 
 int main()
 {
 	int j = 0, sockfd = 0, newsock = 0, n = 0, choice = 0, option = 0;
-	char opt[3], buf[BUF_SIZE], *message[] = { "PAPER\n", "ROCK\n", "SCISSORS\n" };
+	char opt[3], buf[BUF_SIZE];
+	char *message[] = { "PAPER\n", "ROCK\n", "SCISSORS\n" };	  // psr options
 	struct sockaddr_in serv_addr, serv_send;
 	struct hostent *he;
 	ssize_t nread;
@@ -38,13 +37,11 @@ int main()
 	// create a socket
 	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
-		printf("\n ERROR: Could not create socket \n");
-		exit(-1);
+		perror("FATAL: Could not create socket"); exit(-1);
 	}
 	if ((he = gethostbyname(hostname)) == NULL)
 	{
-		printf("\n ERROR: Could not get host by name \n");
-		exit(-1);
+		perror("FATAL: Could not get host by name"); exit(-1);
 	}
 
 	memset(&serv_addr, '\0', sizeof(serv_addr));
@@ -54,8 +51,7 @@ int main()
 
 	if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
 	{
-		perror("\n Error : Connect Failed \n");
-		exit(-1);
+		perror("FATAL: Connect Failed"); exit(-1);
 	}
 
 	// enter a loop and continuously offer psr to the referee
@@ -65,12 +61,11 @@ int main()
 		char command[] = "READY\n";
 		j = write(sockfd, command, sizeof command);
 		// wait for a GO
-		bzero(buf, BUF_SIZE);
+		bzero(buf, BUF_SIZE);							  // zero out the buffer
 		j = read(sockfd, buf, BUF_SIZE);
 		if (j < 0)
 		{
-			perror("ERROR reading from socket");
-			exit(-1);
+			perror("FATAL: reading from socket"); exit(-1);
 		}
 		if (strncmp(buf, "GO", 2) == 0)
 		{
@@ -78,13 +73,7 @@ int main()
 			j = write(sockfd, message[choice], sizeof(message[choice]));
 			if (j < 0)
 			{
-				perror("ERROR writing to socket");
-				exit(-1);
-			}
-			else
-			{
-				// fprintf(stderr, "INFO: Sent choice to referee: %s\n",
-				// message[option]);
+				perror("FATAL: writing to socket"); exit(-1);
 			}
 		}
 		else if (strncmp(buf, "STOP", 4) == 0)
@@ -107,48 +96,9 @@ long random_at_most(long max)
 	num_rand = (unsigned long)RAND_MAX + 1,
 	bin_size = num_rand / num_bins, defect = num_rand % num_bins;
 	long x;
-	do
-	{
-		x = random();
-	}
-	// This is carefully written not to overflow
+	do { x = random(); }
 	while (num_rand - defect <= (unsigned long)x);
 
 	// Truncated division is intentional
 	return x / bin_size % 3;
-}
-
-void readstr(int fd, char buffer[], size_t len)
-{
-	int result;
-	size_t offset = 0;
-	for (;;)
-	{
-		result = recv(fd, &buffer[offset], len, 0);
-		fprintf(stderr, "%d\n", result);
-		if (result < 0)
-		{
-			perror("readStr");
-			exit(EXIT_FAILURE);
-		}
-		if (*buffer == '\n')
-			break;
-		++buffer;
-		len -= result;
-		offset += result;
-	}
-	*buffer = '\0';
-}
-
-int writestr(int fd, char *message)
-{								/* Write formated data */
-	char temp;
-	int length = 0;
-	length = strlen(message) + 1;	/* Find length of string */
-	temp = length;
-	if (write(fd, &temp, 1) < 0)	/* Send length first */
-		exit(-2);
-	if (write(fd, message, length) < 0)	/* Send string */
-		exit(-2);
-	return length;				/* Return size of string */
 }
